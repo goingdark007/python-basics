12fafc1631f7c466f21cb0a33fd2406dafa8be81
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2024-10-13 18:53:30 +0600

    Update header alignment.py

--- header alignment.py
+++ header alignment.py
@@ -10,3 +10,6 @@ print(f"{'Key':<{10}} {'Value':<{10}}")
 #aligns multiple strings space between is 10 spaces
 print('-'*20)
 #prints '-' 20 times
+
+print(f"{'*':^{10}}")
+# aligns * in 10 and ^ is also can be used
